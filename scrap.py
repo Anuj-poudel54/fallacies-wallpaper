e18ec3e2adbd8437cfa22db1a8a33d967c074628
Anuj poudel <anujpoudel54@gmail.com>
2024-06-03 21:20:57 +0545

    Fetched data write to file

--- scrap.py
+++ scrap.py
@@ -47,18 +47,22 @@ while True:
         fallacies_file.close()
 
     short_description = chrome.find_element(
-        By.CSS_SELECTOR, "article.fallacy-detailed h1")
+        By.CSS_SELECTOR, "article.fallacy-detailed h2")
     p_contents = chrome.find_elements(
         By.CSS_SELECTOR, "article.fallacy-detailed .body > p")
 
     # Writing in file
-    fallacies_file.write(f"TITLE: {fallacy_title.text}")
-    fallacies_file.write(f"DESC: {short_description.text}")
-    fallacies_file.write(f"EXT DESC: {p_contents[0].text}")
-    fallacies_file.write(f"EXAMPLE: {p_contents[1].text}")
+    fallacies_file.write(f"TITLE: {fallacy_title.text}\n")
+    fallacies_file.write(f"DESC: {short_description.text}\n")
+    fallacies_file.write(f"EXT DESC: {p_contents[0].text}\n")
+    fallacies_file.write(f"EXAMPLE: {p_contents[1].text}\n")
+    fallacies_file.write("")
 
     # Clicking next button
     next_button = chrome.find_element(By.CSS_SELECTOR, "span.next")
     next_button.click()
+
+    # Again reloading page with next url. Not good way since page should not be reloaded. Need to look into
+    chrome.get(chrome.current_url)
 
 fallacies_file.close()
