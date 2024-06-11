c9c8b69409c42b9cff927643d95fa3dc0fda5b1f
Anuj poudel <anujpoudel54@gmail.com>
2024-06-11 13:08:32 +0545

    added doc string for methods and class

--- file_data_parser.py
+++ file_data_parser.py
@@ -2,11 +2,14 @@ from pathlib import Path
 import json
 
 class FileParser:
+    """ Parser for file """
     def __init__(self, file_path: None | str | Path = None) -> None:
+        """ accepts file_path and not given searches for ./data/fallacies.txt file """
 
         self.file_path = file_path or Path("./data/fallacies.txt")
 
     def get_next_fallacy(self):
+        """ Generator for returning next line from the file """
         with open(self.file_path, "r") as f:
             while fallacy := f.readline(): ## f.readlines() returns "" at eof
                 fallacy = json.loads(fallacy)
